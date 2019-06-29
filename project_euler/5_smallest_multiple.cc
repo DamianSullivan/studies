e8ae0476530abd170f9db0e50e9e5beeb52451d3
Damian Sullivan <djcsdjcs@gmail.com>
2019-06-29 17:38:38 -0500

    Adding smallest multiple.

---
+++ project_euler/5_smallest_multiple.cc
@@ -0,0 +1,14 @@
+// 2520 is the smallest number that can be divided by each of the numbers from 1
+// to 10 without any remainder. What is the smallest positive number that is
+// evenly divisible by all of the numbers from 1 to 20?
+
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+  int smallest = 0;
+  printf("The smallest number divisible by all digits in 1-20 is: %d",
+         smallest);
+}
