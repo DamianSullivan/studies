1858f0a6f5e5a7e97ac537f09187b4900756fa25
Damian Sullivan <djcsdjcs@gmail.com>
2019-06-29 18:02:41 -0500

    Finished smallest multiple.

--- project_euler/5_smallest_multiple.cc
+++ project_euler/5_smallest_multiple.cc
@@ -3,12 +3,31 @@
 // evenly divisible by all of the numbers from 1 to 20?
 
 #include <bits/stdc++.h>
+#include <limits.h>
 #include <stdio.h>
 
 using namespace std;
 
+bool is_divisible(int n, int from, int to) {
+  for (int j = from; j < to; j++) {
+    if (n % j != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int smallest_multiple(int from, int to) {
+  for (int i = 2 * to + 1; i < INT_MAX; i++) {
+    if (is_divisible(i, from, to)) {
+      return i;
+    }
+  }
+  return 0;
+}
+
 int main() {
-  int smallest = 0;
+  int smallest = smallest_multiple(1, 20);
   printf("The smallest number divisible by all digits in 1-20 is: %d",
          smallest);
 }
